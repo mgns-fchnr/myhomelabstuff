a0d27fdb24f25c9fa86113492a1396252268ac64
mgns-fchnr <magnus@mail-mgns.de>
2026-02-28 10:09:40 +0100

    added cloud image

--- vm/test-01.sh
+++ vm/test-01.sh
@@ -1,13 +1,19 @@
 mkdir -p /var/lib/libvirt/images/test-01
+wget -O /var/lib/libvirt/images/test-01/debian-13.qcow2 https://cloud.debian.org/images/cloud/bookworm/latest/debian-13-genericcloud-amd64.qcow2
+cloud-localds seed.iso user-data
 cp /root/VM/test-01/seed.iso /var/lib/libvirt/images/test-01/
 chown libvirt-qemu:libvirt-qemu /var/lib/libvirt/images/test-01/seed.iso
+chown libvirt-qemu:libvirt-qemu /var/lib/libvirt/images/test-01/debian-13.qcow2
 chmod 644 /var/lib/libvirt/images/test-01/seed.iso
+chmod 644 /var/lib/libvirt/images/test-01/debian-13.qcow2
+
+qemu-img resize /var/lib/libvirt/images/test-01/debian-13.qcow2 20G
 
 virt-install \
 --name test-01 \
 --memory 1024 \
 --vcpus 1 \
---disk /var/lib/libvirt/images/test-01.qcow2,format=qcow2,size=10 \
+--disk /var/lib/libvirt/images/test-01/debian-13.qcow2,format=qcow2 \
 --disk /var/lib/libvirt/images/test-01/seed.iso,device=cdrom \
 --os-variant debian13 \
 --network network=default \
