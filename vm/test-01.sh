e34ef0a0c6807933f8ad73a10b8ec06f83d72b99
mgns-fchnr <magnus@mail-mgns.de>
2026-02-28 09:32:43 +0100

    added cp for iso in libvirt folder due to permission issues

--- vm/test-01.sh
+++ vm/test-01.sh
@@ -1,3 +1,8 @@
+mkdir -p /var/lib/libvirt/images/test-01
+cp /root/VM/test-01/seed.iso /var/lib/libvirt/images/test-01/
+chown qemu:qemu /var/lib/libvirt/images/test-01/seed.iso
+chmod 644 /var/lib/libvirt/images/test-01/seed.iso
+
 virt-install \
 --name test-01 \
 --memory 1024 \
