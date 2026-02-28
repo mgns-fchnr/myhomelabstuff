7cd1028c0fa13c240611db6352e650ce0fa4d5d8
mgns-fchnr <magnus@mail-mgns.de>
2026-02-28 22:34:23 +0100

    removed nftables

--- setup_root_server.sh
+++ setup_root_server.sh
@@ -3,20 +3,8 @@ apt update && apt upgrade -y && apt autoremove -y
 cat << EOF >> /root/.bashrc
 alias wasd='clear'
 alias update='apt update && apt upgrade -y && apt autoremove -y'
+alias ll='ls -la'
 EOF
-
-#cat << EOF >> /etc/network/interfaces
-#
-#auto vmbr0
-#iface vmbr0 inet static
-#  address 10.0.10.254/24
-#  bridge_ports none
-#  bridge_stp off
-#  bridge_fd 0
-#  pre-up ip link set vmbr0 up
-#EOF
-
-#systemctl restart networking
 
 cat << EOF >> /etc/environment
 LANG="de_DE.utf-8"
@@ -33,60 +21,6 @@ echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 echo "export PATH=$PATH:/usr/sbin" >> /root/.bashrc
 
 /usr/sbin/sysctl -p
-
-waniface='"eno1"'
-#laniface='"vmbr0"'
-
-cat << EOF >> /etc/nftables.conf
-flush ruleset
-
-table inet filter {
-
-    chain input {
-        type filter hook input priority 0;
-        policy drop;
-
-        # loopback
-        iif lo accept
-
-        # established connections
-        ct state established,related accept
-
-        # allow ssh from lan and wan
-        iif $waniface tcp dport 22 accept
-
-        # allow http and https from wan
-        iif $waniface tcp dport 80 accept
-        iif $waniface tcp dport 443 accept
-
-        # allow ping
-        ip protocol icmp accept
-    }
-
-    chain forward {
-        type filter hook forward priority 0;
-        policy drop;
-    }
-
-    chain output {
-        type filter hook output priority 0;
-        policy accept;
-    }
-}
-
-table ip nat {
-
-    chain postrouting {
-        type nat hook postrouting priority 100;
-
-        # NAT for wan
-        oif $waniface masquerade
-    }
-}
-EOF
-
-systemctl enable nftables
-systemctl start nftables
 
 apt install caddy -y
 
