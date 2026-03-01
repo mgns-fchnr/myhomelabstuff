d639f5a872ed5c54ad673bd4af4afae72c1870d6
mgns-fchnr <magnus@mail-mgns.de>
2026-03-01 16:59:03 +0100

    added destroy and start scripts

---
+++ 00_destroy_all_vms.sh
@@ -0,0 +1,7 @@
+virsh destroy database-01
+virsh destroy docker-01
+virsh destroy docker-02
+virsh destroy k3s-01
+virsh destroy media-01
+virsh destroy mointoring-01
+virsh destroy test-01
---
+++ 00_start_all_vms.sh
@@ -0,0 +1,7 @@
+virsh start database-01
+virsh start docker-01
+virsh start docker-02
+virsh start k3s-01
+virsh start media-01
+virsh start mointoring-01
+virsh start test-01
