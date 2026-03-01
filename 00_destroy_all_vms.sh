f86fb06c37da5dcd2b20bf363f3b39563a0b3a13
mgns-fchnr <magnus@mail-mgns.de>
2026-03-01 17:13:05 +0100

    typo

--- 00_destroy_all_vms.sh
+++ 00_destroy_all_vms.sh
@@ -3,5 +3,5 @@ virsh destroy docker-01
 virsh destroy docker-02
 virsh destroy k3s-01
 virsh destroy media-01
-virsh destroy mointoring-01
+virsh destroy monitoring-01
 virsh destroy test-01
--- 00_start_all_vms.sh
+++ 00_start_all_vms.sh
@@ -3,5 +3,5 @@ virsh start docker-01
 virsh start docker-02
 virsh start k3s-01
 virsh start media-01
-virsh start mointoring-01
+virsh start monitoring-01
 virsh start test-01
