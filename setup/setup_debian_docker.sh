2102a5fabdf0a5de56fc5d5fd05676a0a7d795cf
mgns-fchnr <magnus@mail-mgns.de>
2026-03-01 19:28:27 +0100

    added setup script for docker

--- setup/setup_debian_docker.sh
+++ setup/setup_debian_docker.sh
@@ -16,4 +16,38 @@ localedef -i de_DE -f UTF-8 de_DE.UTF-8
 
 apt install neovim btop -y
 
+apt update
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt update
+
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+mkdir /root/DOCKER
+
+cd /root/DOCKER
+
+apt install cifs-utils
+
+touch /root/.sbox-01
+echo 'user=u469650' >> /root/.sbox-01
+echo 'password=PASSWORD' >> /root/.sbox-01
+
+mkdir /mnt/sbox-01
+echo '//u469650.your-storagebox.de/backup /mnt/sbox-01 cifs credentials=/root/.sbox-01,iocharset=utf8' >> /etc/fstab
+
+system-daemon reload
+mount -a
+
 reboot
